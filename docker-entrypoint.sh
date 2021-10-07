c0e1540498d010e2e0cec5c266bcced4248bc53b
Ari A <DeathByDentures@gmail.com>
2021-10-07 06:30:53 -0500

    Beta6 Version
    Updated to copy config files to persistent storage on first boot.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,12 +2,21 @@
 
 set -e
 
+FILE=/usr/src/ut2004/System/UT2004.ini
+if [ -f "$FILE" ]; then
+    echo "$FILE exists. Moving on..."
+else
+    cp -r /usr/src/System/* /usr/src/ut2004/System
+    cp /usr/src/ut2004/System/UT2004.ini /usr/src/ut2004/System/UT2004.ini.tpl
+    chmod 777 /usr/src/ut2004/System/*
+fi
+
 if [ "$1" = 'ucc-bin' ]; then
     # Put cdkey in place
     echo $CDKEY > cdkey
     if [ -f UT2004.ini.tpl ];
     then
-        envtpl UT2004.ini.tpl   
+        envtpl UT2004.ini.tpl
     fi
 fi
 
