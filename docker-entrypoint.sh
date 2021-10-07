ce64f60bad3a12e55fa0b26e429677cf88cb608b
Ari A <DeathByDentures@gmail.com>
2021-10-07 06:41:00 -0500

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -e
-
 FILE=/usr/src/ut2004/System/UT2004.ini
 if [ -f "$FILE" ]; then
     echo "$FILE exists. Moving on..."
@@ -10,6 +8,8 @@ else
     cp /usr/src/ut2004/System/UT2004.ini /usr/src/ut2004/System/UT2004.ini.tpl
     chmod 777 /usr/src/ut2004/System/*
 fi
+
+set -e
 
 if [ "$1" = 'ucc-bin' ]; then
     # Put cdkey in place
